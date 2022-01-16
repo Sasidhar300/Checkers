2d5af8ead8ee62bb1be8bafda8e926027a64743d
Sasidhar300 <66202996+Sasidhar300@users.noreply.github.com>
2022-01-16 15:39:37 -0500

    Adding two methods required for the game
    One method checks if there is a winner and the other method is responsible for changing the turns of the player

--- src/checker.py
+++ src/checker.py
@@ -1,4 +1,5 @@
 class CheckersGame () :
+    
     def __init__ (self) :
         self.coins = ('empty space','white checker','red checker','white king','red king')
         self.board =  [ [ 0, 2, 0, 2, 0, 2, 0, 2 ]
@@ -13,3 +14,25 @@ class CheckersGame () :
 
         self.whoseMove = "white"
         self.isWon = 0
+  
+     def checkWinner(self) :
+        countw = 0
+        countr = 0
+        for i in self.board:
+            for j in i:
+                if j == 1 or j == 3:
+                    countw += 1
+                elif j == 2 or j == 4:
+                    countr += 1
+                else:
+                    continue
+        if countr == 0:
+            self.isWon = 'white'
+        elif countw == 0:
+            self.isWon = 'red'
+    def changeTurn(self) :
+        if self.whoseMove == "white":
+            self.whoseMove = "red"
+        elif self.whoseMove == "red":
+            self.whoseMove = "white"    
+        
